4ed9e5ee4dc6709af092cbd393ce337018fec022
TheDangerousDevvv <thedangerousyt05@gmail.com>
2025-08-28 19:36:07 -0600

    first commit

---
+++ DODGE/gxap3/__resource.lua
@@ -0,0 +1,79 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+ 
+files {
+    'vehicles1.meta',
+    'carvariations1.meta',
+    'carcols1.meta',
+    'handling1.meta',
+    'vehiclelayouts1.meta',
+	
+
+
+	
+--	'vehicles4.meta',
+--    'carvariations4.meta',
+--    'carcols4.meta',
+--    'handling4.meta',
+--    'vehiclelayouts4.meta',
+	
+--	'vehicles5.meta',
+--    'carvariations5.meta',
+--    'carcols5.meta',
+--    'handling5.meta',
+--    'vehiclelayouts5.meta',
+	
+--	'vehicles6.meta',
+--    'carvariations6.meta',
+--    'carcols6.meta',
+ --   'handling6.meta',
+ --   'vehiclelayouts6.meta',
+	
+--	'vehicles7.meta',
+--   'handling7.meta',
+--    'dlctext7.meta',
+	
+--	'vehicles8.meta',
+--    'carvariations8.meta',
+--    'carcols8.meta',
+--    'handling8.meta',
+--    'vehiclelayouts8.meta',
+	
+--	'vehicles9.meta',
+--    'carvariations9.meta',
+--    'carcols9.meta',
+--    'handling9.meta',
+--    'vehiclelayouts9.meta',
+	
+--	'vehicles10.meta',
+--    'carvariations10.meta',
+--    'carcols10.meta',
+--    'handling10.meta',
+--   'vehiclelayouts10.meta',
+	
+--	'vehicles11.meta',
+--    'carvariations11.meta',
+--    'carcols11.meta',
+--    'handling11.meta',
+--    'vehiclelayouts11.meta',
+	
+--	'vehicles12.meta',
+--    'carvariations12.meta',
+--    'carcols12.meta',
+--    'handling12.meta',
+--  'vehiclelayouts12.meta',
+	
+--	'vehicles13.meta',
+--    'carvariations13.meta',
+--    'carcols13.meta',
+--    'handling13.meta',
+--    'vehiclelayouts13.meta',
+	
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
+
+
